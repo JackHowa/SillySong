0c436fa74a2ee51ad5d37c71d190eefcfa46ded6
jackhowa <johncarverhoward@gmail.com>
2017-06-29 16:44:18 -0500

    initial commit

--- SillySong/ViewController.swift
+++ SillySong/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
+        self.nameField.autocapitalizationType = .words
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        if nameField.text != "" {
+            lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
+    
+    
+    
 }
+
+func shortNameMaker(name: String) -> String {
+    let name = name
+    
+    //    The shortened name should be all lowercase letters
+    var newName = name.lowercased()
+    
+    let vowels : [Character] = ["a","e","i","o","u"]
+    
+    for char in newName.characters{
+        if vowels.contains(char){
+            return newName
+        }
+        newName.remove(at: newName.startIndex)
+    }
+    return newName
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameMaker(name: fullName)
+    
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyrics
+}
+
+
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
 
